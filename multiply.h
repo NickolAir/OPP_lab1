bea491485db3b1bbaf1c71b91c00ac9f8d289efe
Nikolay Ratushnyak <112893665+NickolAir@users.noreply.github.com>
2023-03-02 21:39:54 +0700

    Delete multiply.h

--- multiply.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPP_LAB1_MULTIPLY_H
-#define OPP_LAB1_MULTIPLY_H
-
-double *multiply (double **A, int N, double *x);
-void mult_on_vector(double *x, double *y, int N, int rank, int numprocs, double *res);
-void mult_on_const(double *x, int N, double t);
-
-#endif //OPP_LAB1_MULTIPLY_H
